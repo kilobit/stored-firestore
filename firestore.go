0f131b64fbd1710670dc56d582f380d5dbec1454
Christian Saunders <cps@kilobit.ca>
2019-12-17 10:16:25 -0700

    Implemented store and retrieve operations.

---
+++ firestore.go
@@ -0,0 +1,107 @@
+/* Copyright 2019 Kilobit Labs Inc. */
+
+package firestore // import "kilobit.ca/go/stored-firestore"
+
+import ctx "context"
+import . "kilobit.ca/go/stored"
+import gfs "cloud.google.com/go/firestore"
+//import "google.golang.org/api/iterator"
+import "google.golang.org/api/option"
+
+type Option func(*FireStore)
+
+type Marshaler interface {
+	Marshal(obj Storable) (interface{}, error)
+}
+
+type UnMarshaler interface {
+	UnMarshal(obj interface{}) (Storable, error)
+}
+
+type FireStore struct {
+	project string  // Name of the GCP project
+	client *gfs.Client // Client connection
+	client_opts []option.ClientOption // Client options
+	m Marshaler // Prepare Storables
+	u UnMarshaler // Reconstitute Storables
+}
+
+func NewFireStore(project string, m Marshaler,
+	u UnMarshaler, opts... Option) *FireStore {
+
+	fs := &FireStore{project, nil, []option.ClientOption{}, m, u}
+	
+	fs.Options(opts...)
+
+	return fs
+}
+
+func (fs *FireStore) Options(opts... Option) {
+	for _, opt := range opts {
+		opt(fs)
+	}
+}
+
+func (fs *FireStore) connect() error {
+	if(fs.client != nil) {
+		return nil
+	}
+
+	c, err := gfs.NewClient(
+		ctx.TODO(),
+		fs.project,
+		fs.client_opts...)
+
+	fs.client = c
+	
+	return err
+}
+
+func (fs *FireStore) Close() {
+	fs.client.Close()
+	fs.client = nil
+}
+
+func (fs *FireStore) StoreItem(id ID, obj Storable) error {
+
+	err := fs.connect()
+	if err != nil {
+		return err
+	}
+
+	dr := fs.client.Doc((string)(id))
+	_, err = dr.Set(ctx.TODO(), obj)
+	
+	return err
+}
+
+func (fs *FireStore) Retrieve(id ID) (Storable, error) {
+
+	err := fs.connect()
+	if err != nil {
+		return nil, err
+	}
+
+	dr := fs.client.Doc((string)(id))
+	ds, err := dr.Get(ctx.TODO())
+	if err != nil {
+		return nil, err
+	}
+	
+	return ds.Data(), nil
+}
+
+func (fs *FireStore) List() ([]ID, error) {
+
+	return []ID{}, nil
+}
+
+func (fs *FireStore) Apply(f ItemHandler) error {
+
+	return nil
+}
+
+func (fs *FireStore) Delete(id ID) error {
+
+	return nil
+}
---
+++ firestore_test.go
@@ -0,0 +1,70 @@
+/* Copyright 2019 Kilobit Labs Inc. */
+
+package firestore // import "kilobit.ca/go/stored-firestore"
+
+import . "kilobit.ca/go/stored"
+import "kilobit.ca/go/tested/assert"
+import "testing"
+import "os"
+
+const PROJECT_ENV_NAME string = "GOOGLE_PROJECT_NAME"
+
+func TestFireStoreTest(t *testing.T) {
+	assert.Expect(t, true, true)
+}
+
+func newTestFireStore(t *testing.T) *FireStore {
+	
+	project, ok := os.LookupEnv(PROJECT_ENV_NAME)
+	if !ok {
+		t.Skip(PROJECT_ENV_NAME + " environment variable not set.")
+	}
+	
+	fs := NewFireStore(project, nil, nil)
+
+	return fs
+}
+
+var SNRData map[ID]Storable = map[ID]Storable{
+	"testing/test1": map[string]interface{}{
+		"foo": "bar",
+		"bing": "bong",
+	},
+
+	"testing/test2": map[string]interface{}{
+		"foo": 12,
+		"bing": map[string]interface{}{
+			"nested": true,
+		},
+	},
+
+	"testing/test3": map[string]interface{}{
+		"foo": 42,
+		"bing": 43,
+	},
+}
+
+func TestFireStoreStoreAndRetrieve(t *testing.T) {
+	
+	fs := newTestFireStore(t)
+	defer fs.Close()
+
+	for id, data := range SNRData {
+		
+		err := fs.StoreItem(id, data)
+		if err != nil {
+			t.Errorf("%v\n%s", id, err)
+		}
+
+		obj, err := fs.Retrieve(id)
+		if err != nil {
+			t.Errorf("%v\n%s", id, err)
+		}
+
+		// Useful for debugging.
+		// t.Logf("%#v", data)
+		// t.Logf("%#v", obj)
+		
+		assert.ExpectDeep(t, data, obj)
+	}
+}
